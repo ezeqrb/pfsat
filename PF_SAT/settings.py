db39a3cc5ff3df2e24de507fd3e5b3b1cf010d1e
Serena <serenadituro@gmail.com>
2023-08-30 17:02:50 -0300

    modifico settings

--- PF_SAT/settings.py
+++ PF_SAT/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['15.228.73.227','15.229.11.120','127.0.0.1']
+ALLOWED_HOSTS = ['15.228.73.227','15.229.11.120']
 
 
 # Application definition
